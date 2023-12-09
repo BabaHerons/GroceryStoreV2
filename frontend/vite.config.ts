7e12bb73860e7600bb83759b46e491c78105d5f1
Manmay <manmay.singh@gmail.com>
2023-12-10 03:40:56 +0530

    added PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        cleanupOutdatedCaches: false,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}']
+     },
+      manifest:{
+        name:"Grocery Store",
+        short_name:"GSV2",
+        description:"A store for all your daily needs.",
+        icons: [
+          {
+            src:"/vite.svg",
+            sizes: "512x512",
+            type:"image/svg+xml",
+            purpose:"any maskable"
+          }
+        ]
+      }
+    })
+  ],
 })
